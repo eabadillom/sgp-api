c247d9636e13ed410c62e27e96863cf4ff6b5e3a
Esteban Badillo <ebadillom@gmail.com>
2024-09-09 15:47:28 -0600

    Corrección en la consulta de biometricos por planta.

--- src/main/java/com/ferbo/sgp/api/service/InformacionEmpresaSrv.java
+++ src/main/java/com/ferbo/sgp/api/service/InformacionEmpresaSrv.java
@@ -3,6 +3,8 @@ package com.ferbo.sgp.api.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,9 @@ import com.ferbo.sgp.api.repository.InformacionEmpresaRepo;
 
 @Service
 public class InformacionEmpresaSrv {
+	
+	Logger log = LogManager.getLogger(InformacionEmpresaSrv.class);
+	
 	@Autowired
 	InformacionEmpresaRepo informacionEmpresaRepo;
 	
@@ -29,6 +34,11 @@ public class InformacionEmpresaSrv {
 	public List<InformacionEmpresa> buscarPorIdPlanta(Integer idPlanta) {
 		List<InformacionEmpresa> lista = null;
 		lista = informacionEmpresaRepo.findByIdPlanta(idPlanta);
+		
+		for(InformacionEmpresa model : lista) {
+			log.info("Biometrico: {}", model.getEmpleado().getBiometrico().getIdBiometrico());
+		}
+		
 		return lista;
 	}
 }
