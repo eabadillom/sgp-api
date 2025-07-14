15c2aeac23826478b2bb58d7f8df3f1f3df2a250
JulioMoreno1 <158115372+JulioMoreno1@users.noreply.github.com>
2025-07-14 16:23:30 -0600

    Agregar configuracion e implementacion con Firebase

---
+++ src/main/java/com/ferbo/sgp/api/model/TokenNotificacion.java
@@ -0,0 +1,65 @@
+package com.ferbo.sgp.api.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "det_tok_notif")
+public class TokenNotificacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cd_tok_notif")
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_fp_client", nullable = false)
+    private Sistema sistema;
+
+    @Column(name = "cd_token", length = 300)
+    private String token;
+
+    @Column(name = "st_valido", nullable = false)
+    private Boolean esValido = true;
+
+    // --- Getters y Setters ---
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public Sistema getSistema() {
+		return sistema;
+	}
+
+	public void setSistema(Sistema sistema) {
+		this.sistema = sistema;
+	}
+
+	public Boolean getEsValido() {
+		return esValido;
+	}
+
+	public void setEsValido(Boolean esValido) {
+		this.esValido = esValido;
+	}
+
+}
+
