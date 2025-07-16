a9bfdfaeb94d4cf419ce9cf93a38ef803d48a155
JulioMoreno1 <158115372+JulioMoreno1@users.noreply.github.com>
2025-07-16 12:37:42 -0600

    Agregar logica faltante al token de notificacion

--- src/main/java/com/ferbo/sgp/api/model/TokenNotificacion.java
+++ src/main/java/com/ferbo/sgp/api/model/TokenNotificacion.java
@@ -1,5 +1,6 @@
 package com.ferbo.sgp.api.model;
 
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -61,5 +62,28 @@ public class TokenNotificacion {
 		this.esValido = esValido;
 	}
 
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final TokenNotificacion other = (TokenNotificacion) obj;
+        return Objects.equals(this.id, other.id);
+    }
+
+        
 }
 
