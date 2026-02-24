3146976136ae8b650539d855f6f0483701131655
Esteban Badillo <ebadillom@gmail.com>
2026-02-24 16:40:41 -0600

    Corrección en la relación de empleado - fotografía

--- src/main/java/com/ferbo/sgp/api/service/FotografiaSrv.java
+++ src/main/java/com/ferbo/sgp/api/service/FotografiaSrv.java
@@ -1,5 +1,7 @@
 package com.ferbo.sgp.api.service;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,6 @@ public class FotografiaSrv {
 	FotografiaRepo fotografiaRepository;
 	
 	public Fotografia buscarPorNumeroEmpleado(String numeroEmpleado) {
-		return (Fotografia) fotografiaRepository.findByNumeroEmpleado(numeroEmpleado);
+		return (Fotografia) fotografiaRepository.findByNumeroEmpleado(numeroEmpleado, LocalDate.now());
 	}
 }
